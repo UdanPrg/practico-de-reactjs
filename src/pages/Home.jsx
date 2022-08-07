a021c33225e0c882d7189db8a7b4efae24ac1eaa
Uriel Arana <udan1718@gmail.com>
2022-08-07 17:03:10 -0600

    Reorganizar archivos y carpertas| Add HeaderMenu

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div>
+            <h1>React Shop</h1>
+            <a href="/recovery-password">recovery-password</a>
+            <br />
+            <a href="/login">login</a>
+            <br />
+            <a href="/skdfhkjdsf">Any</a>
+        </div>
+    );
+}
+
+export default Home;
